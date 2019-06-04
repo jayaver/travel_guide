dce5309b47e6999e23af3450458fc987bc343061
Jaya Verma <jayaver@gmail.com>
2019-06-04 11:10:01 +0200

    deletion of signin component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { SigninComponent } from './signin/signin.component';
-import { SignupComponent } from './signup/signup.component'
-import { TravelComponent } from './travel/travel.component';
+import { HomeComponent } from './home/home.component'
+
 
 const routes: Routes = [
-  {path : 'signin', component:SigninComponent},
-  {path: 'Signup', component: SignupComponent},
-  {path: 'travel', component:TravelComponent},
   {path : '', component:HomeComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,17 @@ import { AngularFireDatabaseModule } from '@angular/fire/database'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { SigninComponent } from './signin/signin.component'
-import { SignupComponent } from './signup/signup.component';
 import { environment } from '../environments/environment';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { TravelComponent } from './travel/travel.component'
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SigninComponent,
-    SignupComponent,
     NavComponent,
-    TravelComponent
+    
   ],
   imports: [
     BrowserModule,
