cd287cb87f3aca947df36ac11987a8a66868142f
Deepthy Nair <dpthynair@gmail.com>
2019-06-06 00:09:25 +0200

    wiki added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { WeatherService } from '../weather.service';
 import { NewsService } from '../news.service';
-// import { WikiService } from '../wiki.service';
+import { WikiService } from '../wiki.service';
 
 
 
@@ -14,25 +14,39 @@ import { NewsService } from '../news.service';
   
 // localStorage = [];
 export class HomeComponent implements OnInit {
+  
   terms = [];
-  constructor(private route: ActivatedRoute, private weather: WeatherService, private news:NewsService) { }
+  sources: any;
+  products = []
+  
+  constructor(private route: ActivatedRoute, private weather: WeatherService, private news: NewsService, private wiki: WikiService) { }
 
   ngOnInit() {
     this.route.params.subscribe(term => {
-      console.log(term);
-      this.weather.getCity(term.city).subscribe((term: any) => {
-        //localStorage.setItem('city', 'term.location.name');
-        console.log(term);
-        this.terms = term.terms;
+     // console.log(term);
+      this.weather.getCity(term.city).subscribe((term : any) => {
+        
+        //console.log(term);
+        this.terms = term;
         console.log(this.terms);
         })
 
-      this.news.getCity(term.city).subscribe((term: any) => {
-        // localStorage.setItem('citydata', term.location);
-        console.log(term);
-        // this.terms = term.terms;
-        
-      })
+        this.news.getCity(term.city).subscribe((source: any) => {
+          // localStorage.setItem('citydata', term.location);
+          //console.log(term);
+          console.log(source.articles);
+
+          this.sources = source.articles;
+          console.log(this.sources);
+
+        })
+        this.wiki.getWiki(term.city).subscribe((place: any) => {
+          // localStorage.setItem('citydata', term.location);
+           console.log(place);
+           this.places=place.items[0];
+          console.log(this.places)
+           
+         })
     })
 
     
--- src/app/wiki.service.ts
+++ src/app/wiki.service.ts
@@ -5,5 +5,12 @@ import { Injectable } from '@angular/core';
 })
 export class WikiService {
 
-  constructor() { }
+
+  constructor(private http: HttpClient, private route: ActivatedRoute) { }
+
+  
+
+   getWiki(term: string) {
+   return this.http.get(`https://www.googleapis.com/customsearch/v1?parameters&cx=000274909655342295077:2s1axfsrhrq&q=${term}&key=AIzaSyCjWX0rDHHdGfXT98rwpJeAq5T7jFKdc6A`);
+}
 }
